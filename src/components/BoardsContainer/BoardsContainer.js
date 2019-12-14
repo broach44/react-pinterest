754c9c65235c996780643b080b16956b878e33ee
Crystal Broach <crysland@gmail.com>
2019-12-14 13:22:53 -0600

    boards show down to a single board

--- src/components/BoardsContainer/BoardsContainer.js
+++ src/components/BoardsContainer/BoardsContainer.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import authData from '../../helpers/data/authData';
 import boardsData from '../../helpers/data/boardsData';
 
+import Board from '../Board/Board';
+
 class BoardsContainer extends React.Component {
+  static propTypes = {
+    setSingleBoard: PropTypes.func,
+  }
+
   state = {
     boards: [],
   }
@@ -17,7 +23,11 @@ class BoardsContainer extends React.Component {
   }
 
   render() {
-    return (<div>{this.state.boards.map((board) => <h6>{board.name}</h6>)}</div>);
+    const { setSingleBoard } = this.props;
+    return (
+    <div>
+      {this.state.boards.map((board) => <Board key={board.id} board={board} setSingleBoard={setSingleBoard} />)}
+    </div>);
   }
 }
 

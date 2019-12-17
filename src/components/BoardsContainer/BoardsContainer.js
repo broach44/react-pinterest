677feb031b8b3dacc6dc259646191048679233eb
Crystal Broach <crysland@gmail.com>
2019-12-16 20:41:38 -0600

    able to create new boards

--- src/components/BoardsContainer/BoardsContainer.js
+++ src/components/BoardsContainer/BoardsContainer.js
@@ -4,6 +4,7 @@ import authData from '../../helpers/data/authData';
 import boardsData from '../../helpers/data/boardsData';
 
 import Board from '../Board/Board';
+import BoardForm from '../BoardForm/BoardForm';
 
 class BoardsContainer extends React.Component {
   static propTypes = {
@@ -15,6 +16,10 @@ class BoardsContainer extends React.Component {
   }
 
   componentDidMount() {
+    this.getBoardData();
+  }
+
+  getBoardData = () => {
     boardsData.getBoardsByUid(authData.getUid())
       .then((boards) => {
         this.setState({ boards });
@@ -22,10 +27,20 @@ class BoardsContainer extends React.Component {
       .catch((errfromBoardContainer) => console.error(errfromBoardContainer));
   }
 
+  addBoard = (newBoard) => {
+    boardsData.saveBoard(newBoard)
+      .then(() => {
+        this.getBoardData();
+      })
+      .catch((errorFromSaveBoard) => console.error(errorFromSaveBoard));
+    this.setState({ boardName: '', description: '' });
+  }
+
   render() {
     const { setSingleBoard } = this.props;
     return (
     <div>
+      <BoardForm addBoard={this.addBoard} />
       {this.state.boards.map((board) => <Board key={board.id} board={board} setSingleBoard={setSingleBoard} />)}
     </div>);
   }

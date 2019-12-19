565779569ec996ec612c1cabf6bac6abb3d4fb5a
Crystal Broach <crysland@gmail.com>
2019-12-18 21:08:09 -0600

    update on boards working

--- src/components/MyNavbar/MyNavbar.js
+++ src/components/MyNavbar/MyNavbar.js
@@ -20,7 +20,7 @@ class MyNavbar extends React.Component {
     return (
       <div className="MyNavbar">
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <span className="navbar-brand" href="#">Pinterest</span>
+          <span className="navbar-brand text-info" href="#">Pinterest</span>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -28,7 +28,7 @@ class MyNavbar extends React.Component {
             <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
             </ul>
             <div className="form-inline my-2 my-lg-0">
-            { authed && (<button className="nav-link btn btn-danger" onClick={this.logMeOut}>Logout</button>)}
+            { authed && (<button className="nav-link btn btn-outline-info" onClick={this.logMeOut}>Logout</button>)}
               {/* If NOT authed, show nothing */}
             </div>
           </div>
